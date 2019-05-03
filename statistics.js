f5ccddee3c23dd98b17ffffa1cdd77d1264ac0ba
G3fo <matbarreiros@gmail.com>
2019-05-03 12:04:22 -0300

    update and fixes to htmls

---
+++ statistics.js
@@ -0,0 +1,15 @@
+var statistics = {
+  "numberOfDemocrats" : 0,
+  "numberOfRepublicans" : 0,
+  "numberOfIndependents" : 0,
+  "total" : 0,
+}
+
+
+
+
+var democrats = []
+var republicans = [] 
+var independent = []
+
+
